e82ded80496000dfd8e6d8678c7922b75c868f46
Jeong Tae Bang <jeongtaebang@gmail.com>
2019-11-04 01:39:34 -0500

    connected with heroku

--- App.js
+++ App.js
@@ -13,40 +13,186 @@ import {
   StyleSheet,
   View,
   Text,
-  FlatList,
-  Dimensions
+  Dimensions,
 } from 'react-native';
 
 const blocks = [
-  { key: 'QWER' }, { key: 'TYUI' }, { key: 'OP' }, { key: 'AS' }, { key: 'DFG' }, { key: 'HJKL' }, 
+  { key: 'QWE' }, { key: 'RTYU' }, { key: 'IOP' }, 
+  { key: 'AS' }, { key: 'DFGH' }, { key: 'JKL' }, 
   { key: 'ZXC' }, { key: 'VB' }, { key: 'NM' },
 ]
 
-
-/*
- * Adapted from snack.expo.io@spencercarli/react-native-flatlist-grid
-*/
-const formatData = (data, numCol) => {
-  return data
-};
-
-
+/* constants */
+const vertOffset = 20;
 const numCol = 3;
+const spaceH = 50;
+const THRESH = 700;
+const h = Dimensions.get('window').width / 2 / numCol;
+var coords = [];
+const FLASKURL = "https://swipemini.herokuapp.com/";
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       wordList: [],
-      blockList: [],
     };
+
+    this.recordMove = this.recordMove.bind(this);
+    this.getInterpret = this.getInterpret.bind(this);
+  }
+
+  determineBlockIdx = (x, y) => {
+    if (x < 0 || y < 0) {
+      console.log("Can't process negative coordinates");
+      return;
+    }
+    const xx = Math.floor(x / h);
+    const yy = Math.floor((y - vertOffset) / h );
+
+    var bCode = -1;
+
+    switch (yy) {
+      case 0:
+        if (xx === 0) {
+          bCode = 1;
+          // console.log("Block 1");
+        }
+        if (xx === 1) {
+          bCode = 2;
+          // console.log("Block 2");
+        }
+        if (xx === 2) {
+          bCode = 3;
+          // console.log("Block 3");
+        }
+        break;
+      case 1:
+        if (xx === 0) {
+          bCode = 4;
+          // console.log("Block 4");
+        }
+        if (xx === 1) {
+          bCode = 5;
+          // console.log("Block 5");
+        }
+        if (xx === 2) {
+          bCode = 6;
+          // console.log("Block 6");
+        }
+        break;
+      case 2:
+        if (xx === 0) {
+          bCode = 7;
+          // console.log("Block 7");
+        }
+        if (xx === 1) {
+          bCode = 8;
+          // console.log("Block 8");
+        }
+        if (xx === 2) {
+          bCode = 9;
+          // console.log("Block 9");
+        }
+        break;   
+      default:
+        if (yy === 3 && y < vertOffset + 3*h + spaceH + 1) {
+          bCode = 10;
+          // console.log("Space");
+        }
+        break;
+    }
+    return bCode;
+  };
+
+  recordMove = (evt) => {
+    const x = evt.nativeEvent.pageX;
+    const y = evt.nativeEvent.pageY;
+    const t = evt.nativeEvent.timestamp;
+
+    const blockIdx = this.determineBlockIdx(x, y);
+    if (blockIdx !== -1) {
+      const pt = [blockIdx, t];
+      coords.push(pt);
+    }
+  };
+
+  getInterpret = (evt) => {
+    // console.log(coords);
+    // console.log("End Sequence");
+
+    if (coords.length > 0) {
+      var sanitized = [];
+      var bCode = coords[0][0];
+      var startT = coords[0][1];
+      sanitized.push(bCode);
+
+      coords.map( (val, idx) => {
+        const currB = val[0];
+        const currT = val[1];
+
+        // momemnt of transition
+        if (currB != bCode) {
+          const duration = currT - startT;
+          if (duration >= THRESH) {
+            // console.log(`${currB}: ${currT} - ${startT}`);
+            sanitized.push(currB);
+            startT = currT
+            bCode = currB;
+          }
+        }
+      });
+      
+      const dataLen = coords.length;
+      const sLen= sanitized.length;
+      if (sanitized[sLen - 1] !== coords[dataLen - 1][0]) {
+        sanitized.push(coords[dataLen - 1][0]);    // always include first and last block Code, if appropriate
+      }
+
+      console.log(sanitized);
+      this.getWordsFromFlask(sanitized);
+    }
+
+    coords = [];
+  };
+
+  getWordsFromFlask(arr) {
+    return fetch(FLASKURL, {
+                  method: 'POST',
+                  headers: {
+                    Accept: 'application/json', 
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify({
+                      blocks: arr,
+                  }),
+                })
+            .then((response) => {
+              // console.log(response);
+              return response.json()
+            })
+              .then((responseJson) => {
+                console.log(responseJson);
+                this.setState({
+                  wordList: responseJson['words'],
+                });
+              })
+              .catch((err) => {
+                console.log(err);
+                return null
+              })
+            .catch((err) => {
+              console.log(err);
+              return null;
+            });
   }
 
   renderWords = (words) => {
     let str = "";
     words.map( (word, idx) => {
-      str = str + " " + word;
+        str = str + " " + word;
     });
-
+    
     return str;
   };
 
@@ -55,62 +201,67 @@ export default class App extends React.Component {
     const data = blocks;
     const wordList = this.state.wordList;
     return (
-      <View>
-        {/*<FlatList 
-           data={formatData(blocks, numCol)}
-           style={styles.container}
-           renderItem={this.renderItem}
-           numColumns={numCol}
-        />*/}
-        {/* Blocks 1-3 */}
-        <View style={styles.item1} >
-          <Text style={styles.itemText}> {data[0].key}</Text>
-        </View>
-        <View style={styles.item2} >
-          <Text style={styles.itemText}> {data[1].key} </Text>
-        </View>
-        <View style={styles.item3} >
-          <Text style={styles.itemText}> {data[2].key} </Text>
+      <>
+        <View
+          onStartShouldSetResponder={(evt) => true}
+          onMoveShouldSetResponder={(evt) => true}
+          onResponderTerminationRequest={(evt) => true}
+          onResponderGrant={(evt) => { console.log("Start Sequence"); }}
+          onResponderTerminate={(evt) => { console.log("Responder terminated")}}
+          onResponderMove={this.recordMove}
+          onResponderRelease={this.getInterpret}
+        >
+          {/* Blocks 1-3 */}
+          <View style={styles.item1} >
+            <Text style={styles.itemText}> {data[0].key}</Text>
+          </View>
+          <View style={styles.item2} >
+            <Text style={styles.itemText}> {data[1].key} </Text>
+          </View>
+          <View style={styles.item3} >
+            <Text style={styles.itemText}> {data[2].key} </Text>
+          </View>
+
+          {/* Blocks 4-6 */}
+          <View style={styles.item4} >
+            <Text style={styles.itemText}> {data[3].key}</Text>
+          </View>
+          <View style={styles.item5} >
+            <Text style={styles.itemText}> {data[4].key} </Text>
+          </View>
+          <View style={styles.item6} >
+            <Text style={styles.itemText}> {data[5].key} </Text>
+          </View>
+          
+          {/* Blocks 7-9 */}
+          <View style={styles.item7} >
+            <Text style={styles.itemText}> {data[6].key}</Text>
+          </View>
+          <View style={styles.item8} >
+            <Text style={styles.itemText}> {data[7].key} </Text>
+          </View>
+          <View style={styles.item9} >
+            <Text style={styles.itemText}> {data[8].key} </Text>
+          </View>
+
+          {/* Space */}
+          <View style={styles.space} >
+            <Text style={styles.itemText}> SPACE </Text>
+          </View>
         </View>
 
-        {/* Blocks 4-6 */}
-        <View style={styles.item4} >
-          <Text style={styles.itemText}> {data[3].key}</Text>
+        <View>
+          {/* Words */}
+          <View style={styles.results} >
+            <Text sytle={styles.itemText}> {wordList.length == 0 ? '...' : this.renderWords(wordList)} </Text>
+          </View>
         </View>
-        <View style={styles.item5} >
-          <Text style={styles.itemText}> {data[4].key} </Text>
-        </View>
-        <View style={styles.item6} >
-          <Text style={styles.itemText}> {data[5].key} </Text>
-        </View>
-        
-        {/* Blocks 7-9 */}
-        <View style={styles.item7} >
-          <Text style={styles.itemText}> {data[6].key}</Text>
-        </View>
-        <View style={styles.item8} >
-          <Text style={styles.itemText}> {data[7].key} </Text>
-        </View>
-        <View style={styles.item9} >
-          <Text style={styles.itemText}> {data[8].key} </Text>
-        </View>
-
-        {/* Space */}
-        <View style={styles.space} >
-          <Text style={styles.itemText}> SPACE </Text>
-        </View>
-
-        {/* Words */}
-        <View style={styles.results} >
-          <Text sytle={styles.itemText}> {wordList.length == 0 ? '...' : this.renderWords()} </Text>
-        </View>
-      </View>
+      </>
     );
   }
 }
 
 
-const vertOffset = 20;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -124,9 +275,10 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: 0 + vertOffset,
     left: 0,
-    margin: 1,
-    height: Dimensions.get('window').width / numCol,
-    width: Dimensions.get('window').width / numCol - 2,
+    borderColor: 'white',
+    borderWidth: 1,
+    height: h,
+    width: h,
   },
   item2: {
     backgroundColor: 'skyblue',
@@ -134,10 +286,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     position: 'absolute',
     top: 0 + vertOffset,
-    left: Dimensions.get('window').width / numCol,
-    margin: 1,
-    height: Dimensions.get('window').width / numCol,
-    width: Dimensions.get('window').width / numCol - 2,
+    left: h,
+    borderColor: 'white',
+    borderWidth: 1,
+    height: h,
+    width: h,
   },
   item3: {
     backgroundColor: 'skyblue',
@@ -145,10 +298,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     position: 'absolute',
     top: 0 + vertOffset,
-    left: 2 * Dimensions.get('window').width / numCol,
-    margin: 1,
-    height: Dimensions.get('window').width / numCol,
-    width: Dimensions.get('window').width / numCol - 2,
+    left: 2 * h,
+    borderColor: 'white',
+    borderWidth: 1,
+    height: h,
+    width: h,
   },
 
   item4: {
@@ -156,33 +310,36 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     position: 'absolute',
-    top: vertOffset + Dimensions.get('window').width / numCol + 2,
+    top: vertOffset + h,
     left: 0,
-    margin: 1,
-    height: Dimensions.get('window').width / numCol,
-    width: Dimensions.get('window').width / numCol - 2,
+    borderColor: 'white',
+    borderWidth: 1,
+    height: h,
+    width: h,
   },
   item5: {
     backgroundColor: 'skyblue',
     alignItems: 'center',
     justifyContent: 'center',
     position: 'absolute',
-    top: vertOffset + Dimensions.get('window').width / numCol + 2,
-    left: Dimensions.get('window').width / numCol,
-    margin: 1,
-    height: Dimensions.get('window').width / numCol,
-    width: Dimensions.get('window').width / numCol - 2,
+    top: vertOffset + h,
+    left: h,
+    borderColor: 'white',
+    borderWidth: 1,
+    height: h,
+    width: h,
   },
   item6: {
     backgroundColor: 'skyblue',
     alignItems: 'center',
     justifyContent: 'center',
     position: 'absolute',
-    top: vertOffset + Dimensions.get('window').width / numCol + 2,
-    left: 2 * Dimensions.get('window').width / numCol,
-    margin: 1,
-    height: Dimensions.get('window').width / numCol,
-    width: Dimensions.get('window').width / numCol - 2,
+    top: vertOffset + h,
+    left: 2 * h,
+    borderColor: 'white',
+    borderWidth: 1,
+    height: h,
+    width: h,
   },
 
   item7: {
@@ -190,45 +347,48 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     position: 'absolute',
-    top: vertOffset + 2 * Dimensions.get('window').width / numCol + 4,
+    top: vertOffset + 2 * h,
     left: 0,
-    margin: 1,
-    height: Dimensions.get('window').width / numCol,
-    width: Dimensions.get('window').width / numCol - 2,
+    borderColor: 'white',
+    borderWidth: 1,
+    height: h,
+    width: h,
   },
   item8: {
     backgroundColor: 'skyblue',
     alignItems: 'center',
     justifyContent: 'center',
     position: 'absolute',
-    top: vertOffset + 2 * Dimensions.get('window').width / numCol + 4,
-    left: Dimensions.get('window').width / numCol,
-    margin: 1,
-    height: Dimensions.get('window').width / numCol,
-    width: Dimensions.get('window').width / numCol - 2,
+    top: vertOffset + 2 * h,
+    left: h,
+    borderColor: 'white',
+    borderWidth: 1,
+    height: h,
+    width: h,
   },
   item9: {
     backgroundColor: 'skyblue',
     alignItems: 'center',
     justifyContent: 'center',
     position: 'absolute',
-    top: vertOffset + 2 * Dimensions.get('window').width / numCol + 4,
-    left: 2 * Dimensions.get('window').width / numCol,
-    margin: 1,
-    height: Dimensions.get('window').width / numCol,
-    width: Dimensions.get('window').width / numCol - 2,
+    top: vertOffset + 2 * h,
+    left: 2 * h,
+    borderColor: 'white',
+    borderWidth: 1,
+    height: h,
+    width: h,
   },
 
   space: {
-    width: Dimensions.get('window').width,
-    height: 50,
+    width: Dimensions.get('window').width / 2,
+    height: spaceH,
     backgroundColor: 'skyblue',
     alignItems: 'center',
     justifyContent: 'center',
     position: 'absolute',
-    top: vertOffset + 3 * Dimensions.get('window').width / numCol + 6,
+    top: vertOffset + 3 * h,
     left: 0,
-    margin: 1,
+    marginVertical: 1,
   },
 
   results: {
@@ -238,7 +398,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     position: 'absolute',
-    top: vertOffset + 4 * Dimensions.get('window').width / numCol + 8,
+    top: vertOffset + 4 * h,
     left: 0,
     margin: 3,
   },
